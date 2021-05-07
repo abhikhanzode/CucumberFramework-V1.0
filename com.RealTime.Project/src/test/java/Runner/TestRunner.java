856ff7c1fffe0ec83b403af839e35471dd450ca6
Winjit-RIB-Git <abhijeetk@winjit.com>
2021-05-07 12:45:00 +0530

    "Updated Files, Hooks, Pages, TestContextUI, Steps with PageFactory"

--- com.RealTime.Project/src/test/java/Runner/TestRunner.java
+++ com.RealTime.Project/src/test/java/Runner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 		features = "src/test/resources/Features",
-		glue = {"Steps"},
+		glue = {"Steps","Hooks"},
 		plugin = {"json:target/cucumber-report/cucumber.json"}
 		)
 
